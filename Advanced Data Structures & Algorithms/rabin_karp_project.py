54450982ed5bf6dceb3b5536b017165d2e978731
mazamanuk <mazamanuk@gmail.com>
2023-08-21 12:46:01 +0100

    adding rabin karp implementation with multiple patterns

--- Advanced Data Structures & Algorithms/rabin_karp_project.py
+++ Advanced Data Structures & Algorithms/rabin_karp_project.py
@@ -8,8 +8,26 @@ def polynomial_rolling_hash(previous_hash, c1, c2, pattern_length):
 	return (previous_hash - ord(c1) * (26**(pattern_length - 1))) * 26 + ord(c2)
 
 def rabin_karp_algorithm_multiple(pattern, text):
-	#Your code goes here
-	pass
+	hash_dict = {}
+	occurrence_dict = {}
+	unique_pattern_lengths = set()
+	for string in pattern:
+		string_hash = polynomial_hash(string)
+		hash_dict[string_hash] = string
+		occurrence_dict[string] = 0
+		unique_pattern_lengths.add(len(string))
+	for pattern_length in unique_pattern_lengths:
+		substring_hash = polynomial_hash(text[:pattern_length])
+		if substring_hash in hash_dict:
+			pattern = hash_dict[substring_hash]
+			occurrence_dict[pattern] += 1
+		for character in range(len(text) - pattern_length):
+			previous_hash = substring_hash
+			substring_hash = polynomial_rolling_hash(previous_hash, text[character], text[character + pattern_length], pattern_length)
+			if substring_hash in hash_dict:
+				pattern = hash_dict[substring_hash]
+				occurrence_dict[pattern] += 1
+	return occurrence_dict
 
 patterns = ['ABC', 'BCD', 'CDE', 'DEF']
 text = 'ABCBCDCDEDEF'
